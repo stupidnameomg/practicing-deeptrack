9867b9bbebcc155490d7dbcabe1ae1dfb9f3c87c
muoscar <oscar.muhr@gmail.com>
2022-02-02 10:26:47 +0100

    Added feature map visualization, specifically as a external, importable function.

---
+++ oscar/visualize_feature_maps.py
@@ -0,0 +1,55 @@
+import keras
+
+def visualize_feature_maps(model, image_of_particle, SUBPLOT_SIZE=6, SUBPLOT_WIDTH=8):
+    """
+        # Parameters:
+        model: The keras model whose featuremaps are to be visualized.
+        image_of_particles: The image that should be visualized.
+        SUBPLOT_SIZE: The size of each individual subplot.
+        SUBPLOT_WIDTH: The number of subplots to lign up horizontally. Prefferably a multiple of two.
+    """
+    # Plotting the two input channels (Im() and Re())
+    input_fig, ax = plt.subplots(1, 2)
+
+    ax[0].imshow(image_of_particle[:, :, 0], cmap='gray')
+    ax[0].set_title("Real (?) part")
+    ax[0].set_xticks([])
+    ax[0].set_yticks([])
+
+    ax[1].imshow(image_of_particle[:, :, 1], cmap='gray')
+    ax[1].set_title("Im (?) part")
+    ax[1].set_xticks([])
+    ax[1].set_yticks([])
+
+    plt.show()
+
+    # Plotting all outputs from convolutional layer, each layer is given its own figure
+
+    img = image_of_particle.reshape(1, IMAGE_SIZE, IMAGE_SIZE, 2) # Reshape needed for model prediction input
+
+    for layer in model.layers:
+        if 'conv' not in layer.name: # Ignoring non-convolutional layers
+            continue
+        
+        NUMBER_OF_OUTPUTS = layer.output.shape[3] # Total number of subplots
+        SUBPLOT_HEIGHT = NUMBER_OF_OUTPUTS // SUBPLOT_WIDTH # Number of rows needed
+
+        # Create a submodel equal to the original model but cut off at the current layer.
+        submodel = keras.Model(inputs=model.inputs, outputs=layer.output)
+
+        # Generate prediction outputs for the submodel
+        feature_maps = submodel.predict(img)
+
+        # Plotting all the outputs in a square figure
+
+        output_number = 0
+        fig, ax = plt.subplots(SUBPLOT_HEIGHT, SUBPLOT_WIDTH, figsize=(SUBPLOT_WIDTH * SUBPLOT_SIZE, SUBPLOT_HEIGHT * SUBPLOT_SIZE))
+        fig.suptitle("Output of layer: " + layer.name)
+        ax = ax.flatten()
+
+        for output_number, axis in zip(range(len(ax)), ax):
+            axis.imshow(feature_maps[0, :, :, output_number], cmap='gray')
+            ax[output_number].set_xticks([])
+            ax[output_number].set_yticks([])
+                
+        plt.show()
